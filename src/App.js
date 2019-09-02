50c75d01600c8cc441293334b7522d4981d1fa97
Pier-Luc Brault <plbrault@gmail.com>
2019-09-01 23:42:43 -0400

    Add mechanics to get location

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import getRiding from './getRiding';
 
-const App = () => (
-  <div>Allo</div>
-);
+const App = () => {
+  const [geolocationSupported, setGeolocationSupported] = useState(true);
+  const [geolocationResult, setGeolocationResult] = useState({ status: 'PENDING' });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((location) => {
+        setGeolocationResult({ status: 'OK', location })
+      }, (error) => {
+        if (error.code === error.PERMISSION_DENIED) {
+          setGeolocationResult({ status: 'DENIED' });
+        } else {
+          setGeolocationResult({ status: 'error' });
+        }
+      });
+    } else {
+      setGeolocationSupported(false);
+    }
+  }, [geolocationSupported]);
+
+  if (!geolocationSupported) {
+    return <div>{'La géolocalisation n\'est pas supportée sur ce navigateur.'}</div>;
+  }
+
+  return (
+    <div>
+      {
+        {
+          PENDING: 'PENDING',
+          DENIED: 'DENIED',
+          ERROR: 'ERROR',
+          OK: 'OK',
+        }[geolocationResult.status]
+      }
+    </div>
+  );
+};
 
 export default App;
