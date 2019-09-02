fe28106537c5fdf840a9d58611bb82866893f762
Pier-Luc Brault <plbrault@gmail.com>
2019-09-01 23:15:47 -0400

    Remove initial page content

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import getRiding from './getRiding';
 
-function App() {
-  console.log(getRiding(45.3568551, -71.86294));
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>Allo</div>
+);
 
 export default App;
