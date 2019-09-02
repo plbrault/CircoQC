922d93a5a4456390dee7f5a6ab9fb197650283ad
Pier-Luc Brault <plbrault@gmail.com>
2019-09-02 10:36:23 -0400

    Add Quebec logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+import logo from './logo.svg';
+
 import './App.css';
 
 import getRiding from './getRiding';
@@ -35,6 +37,7 @@ const App = () => {
 
   return (
     <div>
+      <img id="logo" src={logo} alt="Fleur de lys" />
       <h1>Vous êtes dans la circonscription:</h1>
       {
         {
